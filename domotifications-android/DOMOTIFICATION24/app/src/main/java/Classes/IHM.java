97d27a6beef7cf6026c01afe663ca6e2fba4782a
Othman Latyaoui <othmanlatyaoui.pro@gmail.com>
2024-04-03 11:16:28 +0200

    Fichiers manquants de l'application

---
+++ domotifications-android/DOMOTIFICATION24/app/src/main/java/Classes/IHM.java
@@ -0,0 +1,18 @@
+package Classes;
+
+public class IHM {
+
+    private String nomInterface;
+    private String adresseIP;
+
+    // Constructeur
+    public IHM(String nomInterface, String adresseIP) {
+        this.nomInterface = nomInterface;
+        this.adresseIP = adresseIP;
+    }
+
+    // Destructeur
+
+    protected void finalize() throws Throwable {
+    }
+}
---
+++ domotifications-android/DOMOTIFICATION24/app/src/main/java/Classes/Modules.java
@@ -0,0 +1,26 @@
+package Classes;
+public class Modules {
+    private int idModule;
+    private String nomModule;
+    private TypeModule typeModule;
+    private boolean etatActivation;
+    private boolean etatModification;
+    public enum TypeModule {
+        BoiteAuxLettres,
+        Machine,
+        Poubelle
+    }
+
+    // Constructeur
+    public Modules(int idModule, String nomModule, TypeModule typeModule, boolean etatActivation, boolean etatModification) {
+        this.idModule = idModule;
+        this.nomModule = nomModule;
+        this.typeModule = typeModule;
+        this.etatActivation = etatActivation;
+        this.etatModification = etatModification;
+    }
+
+    // Destructeur
+    protected void finalize() throws Throwable {
+    }
+}
