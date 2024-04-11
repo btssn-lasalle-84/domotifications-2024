0522af4c7dbb09a010d6cd192dfa34ccef4d89ce
tvaira <tvaira@free.fr>
2024-04-11 18:44:56 +0200

    Remanie le code

--- domotifications-esp32/lib/StationLumineuse/StationLumineuse.cpp
+++ domotifications-esp32/lib/StationLumineuse/StationLumineuse.cpp
@@ -7,7 +7,6 @@
 
 #include "StationLumineuse.h"
 #include "ServeurWeb.h"
-#include "BandeauLeds.h"
 #include "Boite.h"
 #include "Machine.h"
 #include "Poubelle.h"
@@ -17,40 +16,63 @@
  * @fn StationLumineuse::StationLumineuse
  */
 StationLumineuse::StationLumineuse() :
-    serveurWeb(new ServeurWeb(this)), leds(NB_LEDS, PIN_BANDEAU, NEO_GRB + NEO_KHZ800)
+    serveurWeb(new ServeurWeb(this)),
+    leds(NB_LEDS, PIN_BANDEAU, NEO_GRB + NEO_KHZ800), couleursPoubelles{
+        leds.Color(255, 0, 0),     // Couleur poubelle 0 (rouge)
+        leds.Color(255, 255, 0),   // Couleur poubelle 1 (jaune)
+        leds.Color(0, 0, 255),     // Couleur poubelle 2 (bleue)
+        leds.Color(240, 240, 242), // Couleur poubelle 3 (grise)
+        leds.Color(0, 255, 0)      // Couleur poubelle 4 (verte)
+    }
 {
-    // machine 1
-    machines.push_back(new Machine(1, 0, leds.Color(0, 255, 0), leds));
-    // machine 2
-    machines.push_back(new Machine(2, 1, leds.Color(0, 255, 0), leds));
-    // machine 3
-    machines.push_back(new Machine(3, 2, leds.Color(0, 255, 0), leds));
-    // machine 4
-    machines.push_back(new Machine(4, 3, leds.Color(0, 255, 0), leds));
-    // machine 5
-    machines.push_back(new Machine(5, 4, leds.Color(0, 255, 0), leds));
-    // machine 6
-    machines.push_back(new Machine(6, 5, leds.Color(0, 255, 0), leds));
+    // les machines
+    for(int i = 0; i < NB_LEDS_NOTIFICATION_MACHINES; ++i)
+    {
+        machines.push_back(new Machine(i + 1, i, leds.Color(0, 255, 0), leds));
+    }
 
-    // poubelle rouge
-    poubelles.push_back(new Poubelle(1, 0, leds.Color(255, 0, 0), leds));
-    // poubelle jaune
-    poubelles.push_back(new Poubelle(2, 1, leds.Color(255, 255, 0), leds));
-    // poubelle bleue
-    poubelles.push_back(new Poubelle(3, 2, leds.Color(0, 0, 255), leds));
-    // poubelle grise
-    poubelles.push_back(new Poubelle(4, 3, leds.Color(240, 240, 242), leds));
-    // poubelle verte
-    poubelles.push_back(new Poubelle(5, 4, leds.Color(0, 255, 0), leds));
+    // les poubelles
+    for(int i = 0; i < NB_LEDS_NOTIFICATION_POUBELLES; ++i)
+    {
+        poubelles.push_back(new Poubelle(i + 1, i, couleursPoubelles[i], leds));
+    }
 
-    // boite 1
-    boites.push_back(new Boite(4, 3, leds.Color(255, 0, 0), leds));
-    // boite 2
-    boites.push_back(new Boite(3, 2, leds.Color(255, 0, 0), leds));
-    // boite 3
-    boites.push_back(new Boite(2, 1, leds.Color(255, 0, 0), leds));
-    // boite 4
-    boites.push_back(new Boite(1, 0, leds.Color(255, 0, 0), leds));
+    // les boîtes aux lettres
+    for(int i = 0; i < NB_LEDS_NOTIFICATION_BOITE; ++i)
+    {
+        boites.push_back(new Boite(i + 1, i, leds.Color(255, 0, 0), leds));
+    }
+
+#ifdef TEST_BANDEAU_LEDS
+    // Signale les notifications sur le bandeau
+    for(int i = 0; i < NB_LEDS_NOTIFICATION_MACHINES; ++i)
+    {
+        machines[i]->allumerNotification();
+    }
+    for(int i = 0; i < NB_LEDS_NOTIFICATION_POUBELLES; ++i)
+    {
+        poubelles[i]->allumerNotification();
+    }
+    for(int i = 0; i < NB_LEDS_NOTIFICATION_BOITE; ++i)
+    {
+        boites[i]->allumerNotification();
+    }
+
+    delay(TEST_TEMPORISATION);
+
+    for(int i = 0; i < NB_LEDS_NOTIFICATION_MACHINES; ++i)
+    {
+        machines[i]->eteindreNotification();
+    }
+    for(int i = 0; i < NB_LEDS_NOTIFICATION_POUBELLES; ++i)
+    {
+        poubelles[i]->eteindreNotification();
+    }
+    for(int i = 0; i < NB_LEDS_NOTIFICATION_BOITE; ++i)
+    {
+        boites[i]->eteindreNotification();
+    }
+#endif
 }
 
 /**
@@ -69,19 +91,4 @@ void StationLumineuse::demarrer()
 void StationLumineuse::traiterRequetes()
 {
     serveurWeb->traiterRequetes();
-    poubelles[0]->allumerNotification();
-    poubelles[1]->allumerNotification();
-    poubelles[2]->allumerNotification();
-    poubelles[3]->allumerNotification();
-    poubelles[4]->allumerNotification();
-    machines[0]->allumerNotification();
-    machines[1]->allumerNotification();
-    machines[2]->allumerNotification();
-    machines[3]->allumerNotification();
-    machines[4]->allumerNotification();
-    machines[5]->allumerNotification();
-    boites[0]->allumerNotification();
-    boites[1]->allumerNotification();
-    boites[2]->allumerNotification();
-    boites[3]->allumerNotification();
 }
