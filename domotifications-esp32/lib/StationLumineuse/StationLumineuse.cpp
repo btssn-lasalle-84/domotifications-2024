ad3e68d31456994bef1d553d60aea2a762203bcb
MOUTTE <corentinmoutte@gmail.com>
2024-04-12 11:31:22 +0200

    DOMO-20 création de la boucle pour les leds

--- domotifications-esp32/lib/StationLumineuse/StationLumineuse.cpp
+++ domotifications-esp32/lib/StationLumineuse/StationLumineuse.cpp
@@ -12,6 +12,12 @@
 #include "Machine.h"
 #include "Poubelle.h"
 
+#define ROUGE 255, 0, 0
+#define BLEU  0, 0, 255
+#define VERT  0, 255, 0
+#define JAUNE 255, 255, 0
+#define GRIS  105, 105, 105
+
 /**
  * @brief Constructeur de la classe StationLumineuse
  * @fn StationLumineuse::StationLumineuse
@@ -19,38 +25,34 @@
 StationLumineuse::StationLumineuse() :
     serveurWeb(new ServeurWeb(this)), leds(NB_LEDS, PIN_BANDEAU, NEO_GRB + NEO_KHZ800)
 {
-    // machine 1
-    machines.push_back(new Machine(1, 0, leds.Color(0, 255, 0), leds));
-    // machine 2
-    machines.push_back(new Machine(2, 1, leds.Color(0, 255, 0), leds));
-    // machine 3
-    machines.push_back(new Machine(3, 2, leds.Color(0, 255, 0), leds));
-    // machine 4
-    machines.push_back(new Machine(4, 3, leds.Color(0, 255, 0), leds));
-    // machine 5
-    machines.push_back(new Machine(5, 4, leds.Color(0, 255, 0), leds));
-    // machine 6
-    machines.push_back(new Machine(6, 5, leds.Color(0, 255, 0), leds));
+    for(int i = 0; i < 6; ++i)
+    {
+        int color;
 
-    // poubelle rouge
-    poubelles.push_back(new Poubelle(1, 0, leds.Color(255, 0, 0), leds));
-    // poubelle jaune
-    poubelles.push_back(new Poubelle(2, 1, leds.Color(255, 255, 0), leds));
-    // poubelle bleue
-    poubelles.push_back(new Poubelle(3, 2, leds.Color(0, 0, 255), leds));
-    // poubelle grise
-    poubelles.push_back(new Poubelle(4, 3, leds.Color(240, 240, 242), leds));
-    // poubelle verte
-    poubelles.push_back(new Poubelle(5, 4, leds.Color(0, 255, 0), leds));
-
-    // boite 1
-    boites.push_back(new Boite(4, 3, leds.Color(255, 0, 0), leds));
-    // boite 2
-    boites.push_back(new Boite(3, 2, leds.Color(255, 0, 0), leds));
-    // boite 3
-    boites.push_back(new Boite(2, 1, leds.Color(255, 0, 0), leds));
-    // boite 4
-    boites.push_back(new Boite(1, 0, leds.Color(255, 0, 0), leds));
+        if(i == 0)
+        {
+            color = leds.Color(ROUGE);
+        }
+        else if(i == 1)
+        {
+            color = leds.Color(JAUNE);
+        }
+        else if(i == 2)
+        {
+            color = leds.Color(BLEU);
+        }
+        else if(i == 3)
+        {
+            color = leds.Color(GRIS);
+        }
+        else if(i == 4)
+        {
+            color = leds.Color(VERT);
+        }
+        poubelles.push_back(new Poubelle(i + 1, i, color, leds));
+        machines.push_back(new Machine(i + 1, i, leds.Color(VERT), leds));
+        boites.push_back(new Boite(i + 1, i, leds.Color(ROUGE), leds));
+    }
 }
 
 /**
@@ -69,19 +71,19 @@ void StationLumineuse::demarrer()
 void StationLumineuse::traiterRequetes()
 {
     serveurWeb->traiterRequetes();
-    poubelles[0]->allumerNotification();
-    poubelles[1]->allumerNotification();
-    poubelles[2]->allumerNotification();
-    poubelles[3]->allumerNotification();
-    poubelles[4]->allumerNotification();
     machines[0]->allumerNotification();
-    machines[1]->allumerNotification();
+    // machines[1]->allumerNotification();
     machines[2]->allumerNotification();
-    machines[3]->allumerNotification();
+    // machines[3]->allumerNotification();
     machines[4]->allumerNotification();
-    machines[5]->allumerNotification();
-    boites[0]->allumerNotification();
+    // machines[5]->allumerNotification();
+    poubelles[0]->allumerNotification();
+    // poubelles[1]->allumerNotification();
+    poubelles[2]->allumerNotification();
+    // poubelles[3]->allumerNotification();
+    poubelles[4]->allumerNotification();
+    // boites[0]->allumerNotification();
     boites[1]->allumerNotification();
-    boites[2]->allumerNotification();
+    // boites[2]->allumerNotification();
     boites[3]->allumerNotification();
 }
