572b12a6a38748e00800fd486a456a0da11af89a
tvaira <tvaira@free.fr>
2024-04-12 17:18:37 +0200

    Remet un code fonctionnel !

--- domotifications-esp32/lib/StationLumineuse/StationLumineuse.cpp
+++ domotifications-esp32/lib/StationLumineuse/StationLumineuse.cpp
@@ -31,36 +31,6 @@ StationLumineuse::StationLumineuse() :
         leds.Color(0, 255, 0)      // Couleur poubelle 4 (verte)
     }
 {
-<<<<<<< HEAD
-    for(int i = 0; i < 6; ++i)
-    {
-        int color;
-
-        if(i == 0)
-        {
-            color = leds.Color(ROUGE);
-        }
-        else if(i == 1)
-        {
-            color = leds.Color(JAUNE);
-        }
-        else if(i == 2)
-        {
-            color = leds.Color(BLEU);
-        }
-        else if(i == 3)
-        {
-            color = leds.Color(GRIS);
-        }
-        else if(i == 4)
-        {
-            color = leds.Color(VERT);
-        }
-        poubelles.push_back(new Poubelle(i + 1, i, color, leds));
-        machines.push_back(new Machine(i + 1, i, leds.Color(VERT), leds));
-        boites.push_back(new Boite(i + 1, i, leds.Color(ROUGE), leds));
-    }
-=======
     // les machines
     for(int i = 0; i < NB_LEDS_NOTIFICATION_MACHINES; ++i)
     {
@@ -109,7 +79,6 @@ StationLumineuse::StationLumineuse() :
         boites[i]->eteindreNotification();
     }
 #endif
->>>>>>> 0522af4c7dbb09a010d6cd192dfa34ccef4d89ce
 }
 
 /**
@@ -128,22 +97,4 @@ void StationLumineuse::demarrer()
 void StationLumineuse::traiterRequetes()
 {
     serveurWeb->traiterRequetes();
-<<<<<<< HEAD
-    machines[0]->allumerNotification();
-    // machines[1]->allumerNotification();
-    machines[2]->allumerNotification();
-    // machines[3]->allumerNotification();
-    machines[4]->allumerNotification();
-    // machines[5]->allumerNotification();
-    poubelles[0]->allumerNotification();
-    // poubelles[1]->allumerNotification();
-    poubelles[2]->allumerNotification();
-    // poubelles[3]->allumerNotification();
-    poubelles[4]->allumerNotification();
-    // boites[0]->allumerNotification();
-    boites[1]->allumerNotification();
-    // boites[2]->allumerNotification();
-    boites[3]->allumerNotification();
-=======
->>>>>>> 0522af4c7dbb09a010d6cd192dfa34ccef4d89ce
 }
