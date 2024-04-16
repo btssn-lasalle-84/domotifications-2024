b3929733fb9a30167f390e2e52a8321541ddd37c
tvaira <tvaira@free.fr>
2024-04-16 21:02:21 +0200

    Ajoute les todos

--- domotifications-android/Domotifications/app/src/main/java/com/lasalle/domotifications/Communication.java
+++ domotifications-android/Domotifications/app/src/main/java/com/lasalle/domotifications/Communication.java
@@ -1,12 +1,174 @@
 package com.lasalle.domotifications;
 
-import java.net.InetAddress;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import static android.content.Context.MODE_PRIVATE;
+import static android.content.Context.CONNECTIVITY_SERVICE;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class Communication
 {
-    private InetAddress adresseIP;
+    /**
+     * Constantes
+     */
+    private static final String TAG                      = "_Communication"; //!< TAG pour les logs
+    private static final String ADRESSE_STATION          = "station-lumineuse.local";
+    private static final int    PORT_HTTP                = 80;
+    public final static int CODE_HTTP_REPONSE_JSON = 0; //!< Code indicatif pour les message du Handler
+    /**
+     * Attributs
+     */
+    private static Communication communication =
+      null; //!< Instance unique de Communication (singleton)
+    private OkHttpClient       clientOkHttp = null;
+    private String             adresseStation;
+    private int                numeroPort                  = PORT_HTTP;
+    private String             url                         = null;
+    public static final String PREFERENCES                 = "preferences";
+    public static final String PREFERENCES_ADRESSE_STATION = "adresseIPStation";
+    SharedPreferences          preferences;
 
-    public Communication()
+    private Communication(Context context)
     {
+        this.clientOkHttp = new OkHttpClient();
+        restaurerPreferences(context);
+        // @todo Initialiser url sous la forme http://xxxx:80
+
+        Log.d(TAG, "Communication() url = " + url);
+    }
+
+    private Communication(String adresseStation, Context context)
+    {
+        this.clientOkHttp = new OkHttpClient();
+        restaurerPreferences(context);
+        setAdresseStation(adresseStation);
+        // @todo Initialiser url sous la forme http://xxxx:80/
+        Log.d(TAG, "Communication() url = " + url);
+    }
+
+    public synchronized static Communication getInstance(Context context)
+    {
+        if(communication == null)
+        {
+            communication = new Communication(context);
+        }
+        return communication;
+    }
+
+    public synchronized static Communication getInstance(String adresseStation, Context context)
+    {
+        if(communication == null)
+        {
+            communication = new Communication(adresseStation, context);
+        }
+        return communication;
+    }
+
+    public String getAdresseStation()
+    {
+        return this.adresseStation;
+    }
+
+    public void setAdresseStation(String adresseStation)
+    {
+        if(adresseStation != this.adresseStation)
+        {
+            this.adresseStation = adresseStation;
+            // @todo sauvegarder la valeur dans SharedPreferences
+        }
+    }
+
+    public void emettreRequeteGET(String api, Handler handler)
+    {
+        if(clientOkHttp == null)
+            return;
+        // @todo si l'api ne contient pas le '/' l'ajouter au début
+
+        // @todo créer l'url contenant l'api
+
+        Log.d(TAG, "emettreRequeteGET() url = " + url);
+
+        Request request =
+          new Request.Builder().url(url).addHeader("Content-Type", "application/json").build();
+
+        clientOkHttp.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e)
+            {
+                Log.d(TAG, "emettreRequeteGET() onFailure");
+                e.printStackTrace();
+                // @todo gérer une erreur de requête
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException
+            {
+                Log.d(TAG, "emettreRequeteGET() onResponse - message = " + response.message());
+                Log.d(TAG, "emettreRequeteGET() onResponse - code    = " + response.code());
+
+                if(!response.isSuccessful())
+                {
+                    throw new IOException(response.toString());
+                }
+
+                // la réponse à transmettre à l'emetteur de la requête
+                final String body = response.body().string();
+                Log.d(TAG, "emettreRequeteGET() onResponse - body = " + body);
+                new Thread() {
+                    @Override
+                    public void run()
+                    {
+                        // @todo créer un Message et ajouter le code indicatif dans what et la réponse dans obj
+
+                        // @todo envoyer le Message avec sendMessage() du handler
+                    }
+                }.start();
+            }
+        });
+    }
+
+    private boolean estConnecteReseau(Context context)
+    {
+        ConnectivityManager connectivityManager =
+          (ConnectivityManager)context.getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo == null || !networkInfo.isConnected() ||
+           (networkInfo.getType() != ConnectivityManager.TYPE_WIFI &&
+            networkInfo.getType() != ConnectivityManager.TYPE_MOBILE))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void restaurerPreferences(Context context)
+    {
+        // @todo récupèrer le SharedPreferences à partir du contexte
+
+        // @todo si l'élément existe ?
+        if(preferences.contains(PREFERENCES_ADRESSE_STATION))
+        {
+            // @todo récupèrer l'adresse sauvegardée de la station
+
+        }
+        else
+        {
+            setAdresseStation(ADRESSE_STATION);
+        }
     }
 }
--- domotifications-android/Domotifications/app/src/main/java/com/lasalle/domotifications/FenetrePoubelle.java
+++ domotifications-android/Domotifications/app/src/main/java/com/lasalle/domotifications/FenetrePoubelle.java
@@ -1,7 +1,8 @@
 package com.lasalle.domotifications;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -13,9 +14,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.lasalle.domotifications.R;
-
-import java.util.Vector;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FenetrePoubelle extends AppCompatActivity
 {
@@ -29,6 +30,10 @@ public class FenetrePoubelle extends AppCompatActivity
     // Exemple d'accès à la base de données
     private BaseDeDonnees baseDeDonnees;      //!< Association avec la base de donnees
     private int           nbModulesPoubelles; //!< le nombre de poubelles gérées
+    // Exemple d'accès à la base de données
+    private Communication communication; //!< Association avec la classe Communication
+    private Handler       handler =
+      null; //!< Handler permettant la communication entre la classe Communication et l'activité
     /**
      * GUI
      */
@@ -40,11 +45,11 @@ public class FenetrePoubelle extends AppCompatActivity
     // pour les tests (cf. todo ci-dessous)
     public static final int NB_COULEURS_POUBELLE = 5; //!< Nombre de couleurs max pour les poubelles
     public static final int[] IMAGES_POUBELLES   = {
-        R.drawable.poubelle,
-        R.drawable.poubelle,
-        R.drawable.poubelle,
-        R.drawable.poubelle,
-        R.drawable.poubelle
+          R.drawable.poubelle,
+          R.drawable.poubelle,
+          R.drawable.poubelle,
+          R.drawable.poubelle,
+          R.drawable.poubelle
     }; //!< Id des images des poubelles dans les ressources Android
     // @todo jusqu'à 5 poubelles en couleurs
     /*
@@ -68,6 +73,10 @@ public class FenetrePoubelle extends AppCompatActivity
         baseDeDonnees      = BaseDeDonnees.getInstance(this);
         nbModulesPoubelles = baseDeDonnees.getNbModulesPoubelles();
         Log.d(TAG, "nbModulesPoubelles = " + nbModulesPoubelles);
+
+        initialiserHandler();
+
+        recupererEtats();
 
         initialiserGUI();
     }
@@ -118,5 +127,70 @@ public class FenetrePoubelle extends AppCompatActivity
                 finish();
             }
         });
+    }
+
+    private void recupererEtats()
+    {
+        Log.d(TAG, "recupererEtats()");
+        communication = Communication.getInstance("192.168.1.37", this);
+        communication.emettreRequeteGET("/poubelles", handler);
+    }
+
+    public void traiterReponseJSON(String reponse)
+    {
+        Log.d(TAG, "traiterReponseJSON() reponse = " + reponse);
+        /*
+            Exemple de réponse : pour la requête GET /poubelles
+            body =
+            [
+                {"idPoubelle":1,"couleur":"rouge","etat":false,"actif":true},
+                {"idPoubelle":2,"couleur":"jaune","etat":false,"actif":true},
+                {"idPoubelle":3,"couleur":"bleu","etat":false,"actif":true},
+                {"idPoubelle":4,"couleur":"gris","etat":false,"actif":true},
+                {"idPoubelle":5,"couleur":"vert","etat":false,"actif":true}
+            ]
+        */
+        JSONArray json = null;
+
+        try
+        {
+            json = new JSONArray(reponse);
+            for(int i = 0; i < json.length(); ++i)
+            {
+                JSONObject poubelle   = json.getJSONObject(i);
+                int        idPoubelle = poubelle.getInt("idPoubelle");
+                String     couleur    = poubelle.getString("couleur");
+                Boolean    etat       = poubelle.getBoolean("etat");
+                Boolean    actif      = poubelle.getBoolean("actif");
+                Log.d(TAG,
+                      "traiterReponseJSON() idPoubelle = " + idPoubelle + " couleur = " + couleur +
+                        " etat = " + etat + " actif = " + actif);
+                // @todo Mettre à jour l'IHM
+            }
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void initialiserHandler()
+    {
+        this.handler = new Handler(this.getMainLooper()) {
+            @Override
+            public void handleMessage(Message message)
+            {
+                // Log.d(TAG, "[Handler] message what = " + message.what);
+                // Log.d(TAG, "[Handler] message obj = " + message.obj.toString());
+
+                switch(message.what)
+                {
+                    case Communication.CODE_HTTP_REPONSE_JSON:
+                        Log.d(TAG, "[Handler] REPONSE JSON");
+                        traiterReponseJSON(message.obj.toString());
+                        break;
+                }
+            }
+        };
     }
 }
