ce9c7061e03e15b6a8cf344a03be84e8b2eab535
tvaira <tvaira@free.fr>
2024-04-28 15:54:12 +0200

    Ajoute TODOs

--- domotifications-android/Domotifications/app/src/main/java/com/lasalle/domotifications/FenetrePoubelle.java
+++ domotifications-android/Domotifications/app/src/main/java/com/lasalle/domotifications/FenetrePoubelle.java
@@ -23,24 +23,31 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.Vector;
 
-public class FenetrePoubelle extends AppCompatActivity {
+public class FenetrePoubelle extends AppCompatActivity
+{
     /**
      * Constantes
      */
-    private static final String TAG = "_FenetrePoubelle"; //!< TAG pour les logs
-    private static final String API_GET_POUBELLES = "/poubelles";       //!< TAG pour les logs
+    private static final String TAG                 = "_FenetrePoubelle"; //!< TAG pour les logs
+    private static final String API_GET_POUBELLES   = "/poubelles";       //!< Pour une requête GET
+    private static final String API_PATCH_POUBELLES = "/poubelles"; //!< Pour une requête PATCH
+    private static final int    INTERVALLE          = 1000;         //!< en ms
     /**
      * Attributs
      */
-    // Exemple d'accès à la base de données
-    private BaseDeDonnees baseDeDonnees;      //!< Association avec la base de donnees
-    private int nbModulesPoubelles; //!< le nombre de poubelles gérées
+    private BaseDeDonnees  baseDeDonnees; //!< Association avec la base de donnees
+    private Vector<Module> modulesPoubelles;
+    private int            nbModulesPoubelles; //!< le nombre de poubelles gérées
     // Exemple d'accès à la base de données
     private Communication communication; //!< Association avec la classe Communication
-    private Handler handler =
-            null; //!< Handler permettant la communication entre la classe Communication et l'activité
+    private Handler       handler =
+      null; //!< Handler permettant la communication entre la classe Communication et l'activité
+    private Timer     minuteur               = null;
+    private TimerTask tacheRecuperationEtats = null;
     /**
      * GUI
      */
@@ -51,40 +58,52 @@ public class FenetrePoubelle extends AppCompatActivity {
     public static final int ROUGE = 4; //!< Poubelle rouge
     // pour les tests (cf. todo ci-dessous)
     public static final int NB_COULEURS_POUBELLE = 5; //!< Nombre de couleurs max pour les poubelles
-    public static final int[] IMAGES_POUBELLES = {
-            R.drawable.poubelle_bleue,
-            R.drawable.poubelle_verte,
-            R.drawable.poubelle_jaune,
-            R.drawable.poubelle_grise,
-            R.drawable.poubelle_rouge
+    public static final int[] IMAGES_POUBELLES   = {
+          R.drawable.poubelle_bleue,
+          R.drawable.poubelle_verte,
+          R.drawable.poubelle_jaune,
+          R.drawable.poubelle_grise,
+          R.drawable.poubelle_rouge
     }; //!< Id des images des poubelles dans les ressources Android
-
     private ImageView[] imagesPoubelles; //!< Images des poubelles de couleur
     private ImageButton boutonAccueil;
+    private ImageView[] imagesNotificationPoubelles;
     private Switch[] boutonsActivation;
-    private Vector<Module> modulesPoubelles;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate()");
 
+        initialiserBaseDeDonnees();
+        initialiserModulesPoubelles();
+        initialiserGUI();
+        initialiserHandler();
+        initialiserMinuteur();
+        initialiserCommunication();
+        recupererEtats();
+    }
+
+    private void initialiserBaseDeDonnees()
+    {
+        Log.d(TAG, "initialiserBaseDeDonnees()");
         baseDeDonnees = BaseDeDonnees.getInstance(this);
-        modulesPoubelles = baseDeDonnees.getPoubelles();
+    }
+
+    private void initialiserModulesPoubelles()
+    {
+        Log.d(TAG, "initialiserModulesPoubelles()");
+        modulesPoubelles   = baseDeDonnees.getPoubelles();
         nbModulesPoubelles = baseDeDonnees.getNbModulesPoubelles();
         Log.d(TAG, "nbModulesPoubelles = " + nbModulesPoubelles);
-
-        initialiserGUI();
-
-        initialiserHandler();
-
-        recupererEtats();
     }
 
     /**
      * @brief Initialise les ressources graphiques de l'activité
      */
-    private void initialiserGUI() {
+    private void initialiserGUI()
+    {
         // contenu bord à bord
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_poubelle);
@@ -95,73 +114,210 @@ public class FenetrePoubelle extends AppCompatActivity {
         });
         Log.d(TAG, "initialiserGUI()");
 
-        boutonAccueil = (ImageButton) findViewById(R.id.boutonAccueil);
+        boutonAccueil = (ImageButton)findViewById(R.id.boutonAccueil);
 
-        imagesPoubelles = new ImageView[NB_COULEURS_POUBELLE];
-        imagesPoubelles[0] = (ImageView) findViewById(R.id.poubelle0);
-        imagesPoubelles[1] = (ImageView) findViewById(R.id.poubelle1);
-        imagesPoubelles[2] = (ImageView) findViewById(R.id.poubelle2);
-        imagesPoubelles[3] = (ImageView) findViewById(R.id.poubelle3);
-        imagesPoubelles[4] = (ImageView) findViewById(R.id.poubelle4);
+        imagesPoubelles    = new ImageView[NB_COULEURS_POUBELLE];
+        imagesPoubelles[0] = (ImageView)findViewById(R.id.poubelle0);
+        imagesPoubelles[1] = (ImageView)findViewById(R.id.poubelle1);
+        imagesPoubelles[2] = (ImageView)findViewById(R.id.poubelle2);
+        imagesPoubelles[3] = (ImageView)findViewById(R.id.poubelle3);
+        imagesPoubelles[4] = (ImageView)findViewById(R.id.poubelle4);
 
-        boutonsActivation = new Switch[NB_COULEURS_POUBELLE];
-        boutonsActivation[0] = (Switch) findViewById(R.id.activationPoubelle0);
-        boutonsActivation[1] = (Switch) findViewById(R.id.activationPoubelle1);
-        boutonsActivation[2] = (Switch) findViewById(R.id.activationPoubelle2);
-        boutonsActivation[3] = (Switch) findViewById(R.id.activationPoubelle3);
-        boutonsActivation[4] = (Switch) findViewById(R.id.activationPoubelle4);
+        imagesNotificationPoubelles    = new ImageView[NB_COULEURS_POUBELLE];
+        imagesNotificationPoubelles[0] = (ImageView)findViewById(R.id.notificationPoubelle0);
+        imagesNotificationPoubelles[1] = (ImageView)findViewById(R.id.notificationPoubelle1);
+        imagesNotificationPoubelles[2] = (ImageView)findViewById(R.id.notificationPoubelle2);
+        imagesNotificationPoubelles[3] = (ImageView)findViewById(R.id.notificationPoubelle3);
+        imagesNotificationPoubelles[4] = (ImageView)findViewById(R.id.notificationPoubelle4);
 
-        for (int i = 0; i < NB_COULEURS_POUBELLE; ++i) {
+        boutonsActivation    = new Switch[NB_COULEURS_POUBELLE];
+        boutonsActivation[0] = (Switch)findViewById(R.id.activationPoubelle0);
+        boutonsActivation[1] = (Switch)findViewById(R.id.activationPoubelle1);
+        boutonsActivation[2] = (Switch)findViewById(R.id.activationPoubelle2);
+        boutonsActivation[3] = (Switch)findViewById(R.id.activationPoubelle3);
+        boutonsActivation[4] = (Switch)findViewById(R.id.activationPoubelle4);
+
+        for(int i = 0; i < NB_COULEURS_POUBELLE; ++i)
+        {
             imagesPoubelles[i].setImageResource(IMAGES_POUBELLES[i]);
+            final int numeroPoubelle = i;
+            imagesNotificationPoubelles[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    gererClicBoutonNotification(numeroPoubelle);
+                }
+            });
+            imagesPoubelles[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    gererClicBoutonNotification(numeroPoubelle);
+                }
+            });
         }
 
-        for (int i = 0; i < NB_COULEURS_POUBELLE; ++i) {
+        for(int i = 0; i < NB_COULEURS_POUBELLE; ++i)
+        {
             imagesPoubelles[i].setVisibility(View.INVISIBLE);
             boutonsActivation[i].setVisibility(View.INVISIBLE);
         }
 
-        for (int i = 0; i < nbModulesPoubelles; ++i) {
+        for(int i = 0; i < nbModulesPoubelles; ++i)
+        {
             imagesPoubelles[i].setVisibility(View.VISIBLE);
             boutonsActivation[i].setVisibility(View.VISIBLE);
         }
 
-        for (int i = 0; i < nbModulesPoubelles; i++) {
+        for(int i = 0; i < nbModulesPoubelles; i++)
+        {
             final int numeroPoubelle = i;
+            // initialise la GUI en fonction de l'état de notification
+            if(modulesPoubelles.get(i).estNotifie())
+            {
+                imagesNotificationPoubelles[i].setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                imagesNotificationPoubelles[i].setVisibility(View.INVISIBLE);
+            }
             // initialise la GUI en fonction de l'état d'activation
-            if (modulesPoubelles.get(i).estActif()) {
+            if(modulesPoubelles.get(i).estActif())
+            {
                 boutonsActivation[i].setChecked(true);
+                imagesNotificationPoubelles[i].setEnabled(true);
                 imagesPoubelles[i].setEnabled(true);
-            } else {
+            }
+            else
+            {
                 boutonsActivation[i].setChecked(false);
+                imagesNotificationPoubelles[i].setEnabled(false);
                 imagesPoubelles[i].setEnabled(false);
             }
             boutonsActivation[i].setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
+                public void onClick(View v)
+                {
                     gererClicBoutonActivation(numeroPoubelle);
                 }
             });
         }
 
-        boutonAccueil = (ImageButton) findViewById(R.id.boutonAccueil);
+        boutonAccueil = (ImageButton)findViewById(R.id.boutonAccueil);
         boutonAccueil.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 Log.d(TAG, "clic boutonAccueil");
+                if(minuteur != null)
+                    minuteur.cancel();
                 finish();
             }
         });
     }
 
-    private void recupererEtats() {
-        Log.d(TAG, "recupererEtats()");
+    private void initialiserHandler()
+    {
+        this.handler = new Handler(this.getMainLooper()) {
+            @Override
+            public void handleMessage(Message message)
+            {
+                // Log.d(TAG, "[Handler] message what = " + message.what);
+                // Log.d(TAG, "[Handler] message obj = " + message.obj.toString());
+
+                switch(message.what)
+                {
+                    case Communication.CODE_HTTP_REPONSE_JSON:
+                        Log.d(TAG, "[Handler] REPONSE JSON");
+                        traiterReponseJSON(message.obj.toString());
+                        break;
+                    case Communication.CODE_HTTP_ERREUR:
+                        Log.d(TAG, "[Handler] ERREUR HTTP");
+                        // @todo gérer l'erreur (au moins afficher un Toast)
+                        break;
+                }
+            }
+        };
+    }
+
+    private void initialiserMinuteur()
+    {
+        minuteur = new Timer();
+    }
+
+    private void initialiserCommunication()
+    {
         // On récupère l'URL de la station dans la base de données
         // communication = Communication.getInstance(this);
         // ou on indique l'adresse de la station :
         communication = Communication.getInstance(Communication.ADRESSE_IP_STATION, this);
-        communication.emettreRequeteGET(API_GET_POUBELLES, handler);
     }
 
-    public void traiterReponseJSON(String reponse) {
+    private void gererClicBoutonActivation(int numeroPoubelle)
+    {
+        if(modulesPoubelles.get(numeroPoubelle) == null)
+        {
+            Log.e(TAG, "Aucune poubelle !");
+            return;
+        }
+        Log.d(TAG,
+              "gererClicBoutonActivation() numeroPoubelle = " + numeroPoubelle +
+                " activation = " + boutonsActivation[numeroPoubelle].isChecked());
+
+        modulesPoubelles.get(numeroPoubelle)
+          .setActif(boutonsActivation[numeroPoubelle].isChecked());
+
+        if(modulesPoubelles.get(numeroPoubelle).estActif())
+        {
+            imagesPoubelles[numeroPoubelle].setEnabled(true);
+            // @todo rendre la cloche active
+        }
+        else
+        {
+            imagesPoubelles[numeroPoubelle].setEnabled(false);
+            // @todo rendre la cloche inactive
+        }
+    }
+
+    private void gererClicBoutonNotification(int numeroPoubelle)
+    {
+        if(modulesPoubelles.get(numeroPoubelle) == null)
+        {
+            Log.e(TAG, "Aucune poubelle !");
+            return;
+        }
+        Log.d(TAG,
+              "gererClicBoutonNotification() numeroPoubelle = " + numeroPoubelle +
+                " idPoubelle = " + modulesPoubelles.get(numeroPoubelle).getIdModule() +
+                " notification = " + modulesPoubelles.get(numeroPoubelle).estNotifie() +
+                " activation = " + modulesPoubelles.get(numeroPoubelle).estActif());
+
+        // @todo si le module est activé
+        // @todo alors si le module est noitifié
+        // @todo alors envoyer l'acquittement de la notification avec une requête PATCH
+        /*
+            Exemple :
+            $ curl --location 'http://station-lumineuse.local:80/poubelles/1' --request PATCH
+           --header 'Content-Type: application/json' --data '{"idPoubelle": "1","etat": false}'
+         */
+        // @todo et enregistrer l'acquittement de la notification dans la base de données
+    }
+
+    private void recupererEtats()
+    {
+        Log.d(TAG, "recupererEtats()");
+        tacheRecuperationEtats = new TimerTask() {
+            public void run()
+            {
+                communication.emettreRequeteGET(API_GET_POUBELLES, handler);
+            }
+        };
+
+        // toutes les secondes (en attente de l'implémentation de la websocket)
+        minuteur.schedule(tacheRecuperationEtats, INTERVALLE, INTERVALLE);
+    }
+
+    public void traiterReponseJSON(String reponse)
+    {
         Log.d(TAG, "traiterReponseJSON() reponse = " + reponse);
         /*
             Exemple de réponsee : pour la requête GET /poubelles
@@ -176,75 +332,46 @@ public class FenetrePoubelle extends AppCompatActivity {
         */
         JSONArray json = null;
 
-        try {
+        try
+        {
             json = new JSONArray(reponse);
-            for (int i = 0; i < json.length(); ++i) {
-                JSONObject poubelle = json.getJSONObject(i);
-                int idPoubelle = poubelle.getInt("idPoubelle");
-                String couleur = poubelle.getString("couleur");
-                Boolean etat = poubelle.getBoolean("etat");
-                Boolean actif = poubelle.getBoolean("actif");
+            for(int i = 0; i < json.length(); ++i)
+            {
+                JSONObject poubelle   = json.getJSONObject(i);
+                int        idPoubelle = poubelle.getInt("idPoubelle");
+                String     couleur    = poubelle.getString("couleur");
+                Boolean    etat       = poubelle.getBoolean("etat");
+                Boolean    actif      = poubelle.getBoolean("actif");
                 Log.d(TAG,
-                        "traiterReponseJSON() idPoubelle = " + idPoubelle + " couleur = " + couleur +
-                                " etat = " + etat + " actif = " + actif);
-                for (Module module : modulesPoubelles) {
-                    if (module.getIdModule() == idPoubelle) {
+                      "traiterReponseJSON() idPoubelle = " + idPoubelle + " couleur = " + couleur +
+                        " etat = " + etat + " actif = " + actif);
+                for(int j = 0; j < modulesPoubelles.size(); ++j)
+                {
+                    Module module = modulesPoubelles.get(j);
+                    if(module.getIdModule() == idPoubelle &&
+                       module.getTypeModule() == Module.TypeModule.Poubelle)
+                    {
                         module.setCouleur(couleur);
-                        module.setTypeModule(Module.TypeModule.Poubelle);
                         module.setActif(actif);
+                        module.setEtatNotification(etat);
+                        mettreAJourModule(j);
                         break;
                     }
                 }
-
-
-                // @todo Mettre à jour l'IHM
-
-
             }
-        } catch (JSONException e) {
+        }
+        catch(JSONException e)
+        {
             e.printStackTrace();
         }
     }
 
-    private void initialiserHandler() {
-        this.handler = new Handler(this.getMainLooper()) {
-            @Override
-            public void handleMessage(Message message) {
-                // Log.d(TAG, "[Handler] message what = " + message.what);
-                // Log.d(TAG, "[Handler] message obj = " + message.obj.toString());
-
-                switch (message.what) {
-                    case Communication.CODE_HTTP_REPONSE_JSON:
-                        Log.d(TAG, "[Handler] REPONSE JSON");
-                        traiterReponseJSON(message.obj.toString());
-                        break;
-                }
-            }
-        };
-    }
-
-    private void gererClicBoutonActivation(int numeroPoubelle) {
-        Log.d(TAG,
-                "gererClicBoutonActivation() numeroPoubelle = " + numeroPoubelle +
-                        " activation = " + boutonsActivation[numeroPoubelle].isChecked());
-        if (modulesPoubelles.get(numeroPoubelle) == null) {
-            Log.e(TAG, "Aucune poubelle !");
-            return;
-        }
-
-        modulesPoubelles.get(numeroPoubelle)
-                .setActif(boutonsActivation[numeroPoubelle].isChecked());
-
-        if (modulesPoubelles.get(numeroPoubelle).estActif()) {
-            baseDeDonnees.mettreAJourEtatActivationModule(
-                    modulesPoubelles.get(numeroPoubelle).getIdModule(),
-                    true);
-            imagesPoubelles[numeroPoubelle].setEnabled(true);
-        } else {
-            baseDeDonnees.mettreAJourEtatActivationModule(
-                    modulesPoubelles.get(numeroPoubelle).getIdModule(),
-                    false);
-            imagesPoubelles[numeroPoubelle].setEnabled(false);
-        }
+    private void mettreAJourModule(int numeroPoubelle)
+    {
+        // @todo si le module est activé
+        // @todo si le module est notifié
+        // @todo alors rendre visible la cloche correspondante
+        // @todo et rendre les boutons actifs et signaler l'activation
+        // @todo sinon rendre les boutons inactifs et signaler l'inactivation
     }
 }
