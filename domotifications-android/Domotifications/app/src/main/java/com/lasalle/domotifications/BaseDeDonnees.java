e08747b6959e1e3a7f236ab7ccceea8cd7e77a03
OthmanLatyaoui <othmanlatyaoui.pro@gmail.com>
2024-06-01 20:01:31 +0200

    Couleurs en hexa dans la BDD

--- domotifications-android/Domotifications/app/src/main/java/com/lasalle/domotifications/BaseDeDonnees.java
+++ domotifications-android/Domotifications/app/src/main/java/com/lasalle/domotifications/BaseDeDonnees.java
@@ -406,21 +406,21 @@ public class BaseDeDonnees extends SQLiteOpenHelper
         sqlite.execSQL(
           "INSERT INTO domotifications(nom, urlServeurWeb, urlServeurWebsocket, nbBoitesAuxLettres, nbPoubelles, nbMachines) VALUES ('BTS', 'http://station-lumineuse.local:80', 'ws://station-lumineuse.local:5000', 1, 5, 6);");
         sqlite.execSQL(
-          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (1, 'boîte aux lettres', 1, 1, 'rouge', 1);");
+          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (1, 'boîte aux lettres', 1, 1, '#FF0000', 1);");
         sqlite.execSQL(
-          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (1, 'bleue', 2, 1, 'bleu', 1);");
+          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (1, 'bleue', 2, 1, '#0000FF', 1);");
         sqlite.execSQL(
-          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (2, 'verte', 2, 0, 'vert', 1);");
+          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (2, 'verte', 2, 0, '#00FF00', 1);");
         sqlite.execSQL(
-          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (3, 'jaune', 2, 0, 'jaune', 1);");
+          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (3, 'jaune', 2, 0, '#FFFF00', 1);");
         sqlite.execSQL(
-          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (4, 'grise', 2, 0, 'gris', 1);");
+          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (4, 'grise', 2, 0, '#F0F0F2', 1);");
         sqlite.execSQL(
-          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (5, 'rouge', 2, 0, 'rouge', 1);");
+          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (5, 'rouge', 2, 0, '#FF0000', 1);");
         sqlite.execSQL(
-          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (1, 'machine à laver', 3, 1, 'orange', 1);");
+          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (1, 'machine à laver', 3, 1, '#FF7F00', 1);");
         sqlite.execSQL(
-          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (2, 'lave-vaisselle', 3, 1, 'rose', 1);");
+          "INSERT INTO modules (id, nom, idTypesModules, actif, couleur, idDomotifications) VALUES (2, 'lave-vaisselle', 3, 1, '#EEC4C9', 1);");
     }
 
     /**
